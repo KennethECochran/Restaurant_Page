ac6e0153770cda0f759be57cf991cf8d0e029ec6
KennethC <91973026+KennethECochran@users.noreply.github.com>
2023-10-15 16:35:44 -0400

    Added basic initial styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './style.css';
+import Icon from './cupcake-1.jpeg';
 
 console.log("Message recieved");
+const element = document.getElementById('landing');
+
+function addImg(){
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    return myIcon;
+}
+
+function landingText(){
+    const content = document.createElement('div');
+    content.classList.add('text');
+    content.textContent = 'Ken\'s cakes offer premium cakes for any occasion! From weddings to birthdays, a cake for any occasion- you need Ken\'s cakes!'
+    return content;
+}
+
+element.appendChild(addImg());
+element.appendChild(landingText());
